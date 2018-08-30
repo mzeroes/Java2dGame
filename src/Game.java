3a73583000959892be1c7e9a547fc18fbffe8733
Shubham Jain <s.shubjain@gmail.com>
2018-08-31 05:25:32 +0530

    checkpoint

---
+++ src/Game.java
@@ -0,0 +1,127 @@
+import graphics.Screen;
+import inputs.Keyboard;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+public class Game extends Canvas implements Runnable {
+    private static final long serialVersionUID = 1L;
+
+    public static int width = 256;
+    public static int height = width / 16 * 9;
+    public static int scale = 3;
+
+    private Thread thread;
+    public static String Title = "My Java Game";
+    private JFrame frame;
+    private Keyboard key;
+    Screen screen = new Screen(width, height);
+
+    public Game() {
+        Dimension size = new Dimension(width * scale, height * scale);
+        setPreferredSize(size);
+        frame = new JFrame();
+        key = new Keyboard();
+        addKeyListener(key);
+    }
+
+    private boolean running = false;
+    private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+
+
+    public synchronized void start() {
+        running = true;
+        thread = new Thread(this, "display");
+        thread.start();
+    }
+
+    public synchronized void stop() {
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void run() {
+        long lastTime = System.nanoTime();
+        final double ns = 1000000000.0 / 60.0;
+        double delta = 0;
+        int frames = 0;
+        int updates = 0;
+        long timer = System.currentTimeMillis();
+        while (running) {
+            // game loop
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while (delta >= 1) {
+                update();
+                updates++;
+                delta--;
+            }
+            render();
+            frames++;
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                frame.setTitle(Title + " | " + updates + "ups, " + frames + "fps");
+                updates = 0;
+                frames = 0;
+            }
+        }
+        stop();
+    }
+    int x = 0 , y =0;
+    public void update() {
+        key.update();
+        if (key.up) y--;
+        if (key.down) y++;
+        if (key.right) x--;
+        if (key.left) x++;
+    }
+
+    public void render() {
+        // buffer strategy ->
+        BufferStrategy bs = getBufferStrategy();
+        if (bs == null) {
+            createBufferStrategy(3);
+            return;
+        }
+        screen.clear();
+        screen.render(x,y);
+
+        for (int i = 0; i < pixels.length; i++) {
+            pixels[i] = screen.pixels[i];
+        }
+        Graphics g = bs.getDrawGraphics();
+        {
+            // do graphics here
+//            g.setColor(Color.BLACK);
+//            g.fillRect(0, 0, getWidth(), getHeight());
+            g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+
+        }
+        g.dispose(); // it releases res.
+        bs.show();
+
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.frame.setResizable(false);
+        game.frame.setTitle("This Game is Shit!");
+        game.frame.add(game);
+        game.frame.pack();
+        game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        game.frame.setLocationRelativeTo(null);
+        game.frame.setVisible(true);
+        game.start();
+    }
+}
